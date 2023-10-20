b4c50390996f3ec0f799359c38d012bb192b1e05
Eric Fultz <eric.fultz@productops.com>
2023-10-20 15:29:39 -0700

    more lax on vid timing

--- vector/thalos_vids_exist.py
+++ vector/thalos_vids_exist.py
@@ -11,7 +11,7 @@ from pathlib import Path
 import os
 import json
 
-MAGIC_12_MINUTES_IN_SECONDS = 12*60.0
+MAGIC_20_MINUTES_IN_SECONDS = 20*60.0
 
 from flask.config import Config as FlaskConfig
 flaskconfig = FlaskConfig(root_path='')
@@ -40,7 +40,7 @@ class ThalosVideosExistVector():
 
     def execute(self, expected_timedelta: timedelta):
 
-        if time.monotonic() < MAGIC_12_MINUTES_IN_SECONDS:
+        if time.monotonic() < MAGIC_20_MINUTES_IN_SECONDS:
             # to recent from system boot time. don't run.
             return
 
@@ -49,8 +49,9 @@ class ThalosVideosExistVector():
 
         nowfloor5min = now.replace(minute=(now.minute//5)*5, second=0, microsecond=0)
         nowfloorminus10min = nowfloor5min - timedelta(minutes=5)
+        nowfloorminus15min = nowfloor5min - timedelta(minutes=10)
         
-        nowstr = nowfloorminus10min.strftime('%d-%m-%Y-%H-%M')
+        nowstr = nowfloorminus15min.strftime('%d-%m-%Y-%H-%M')
         result = Test(name="thalos video files check, looking for %s "%(nowstr), vector=self.rv)
 
 
@@ -58,16 +59,29 @@ class ThalosVideosExistVector():
         self.session.add(result)
         self.session.commit()
         result.score = 1.0
+        errors = ""
         for cam in ['cam1', 'cam2']:
             try:
-                mp4vid = Path(thalosviddir + '/' + cam + '/' + nowfloorminus10min.strftime('%d-%m-%Y') + '/' + nowfloorminus10min.strftime('%H') + '/' + nowstr + ".mp4.done")
+                mp4vid = Path(thalosviddir + '/' + cam + '/' + nowfloorminus15min.strftime('%d-%m-%Y') + '/' + nowfloorminus15min.strftime('%H') + '/' + nowstr + ".mp4.done")
                 st = mp4vid.stat()
-                
-                if st.st_size > 1000000:
-                    result.score -= 0.5
+                # score based on size? I guess? larger than 1MiB is like 65% confident that the file is ok
+                if st.st_size > 0:
+                    result.score -= 0.25 * ( 1.0 - (1.0 / (1.0 +  st.st_size / 500000.0 )) )
             except Exception as e:
                 print("error", type(e), e)
-                result.detail = str(e)
+                errors += str(e)
+            try:
+                mp4vid = Path(thalosviddir + '/' + cam + '/' + nowfloorminus10min.strftime('%d-%m-%Y') + '/' + nowfloorminus10min.strftime('%H') + '/' + nowstr + ".avi.done")
+                st = mp4vid.stat()
+                
+                # score based on size? I guess? larger than 1MiB is like 65% confident that the file is ok
+                if st.st_size > 0:
+                    result.score -= 0.25 * ( 1.0 - (1.0 / (1.0 +  st.st_size / 500000.0 )) )
+            except Exception as e:
+                print("error", type(e), e)
+                errors += str(e)
+        if len(errors)> 0:
+            result.detail = errors
         self.session.commit()
 
         return result
